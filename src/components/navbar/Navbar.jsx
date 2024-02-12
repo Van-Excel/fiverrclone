9d486799caf145a6078032f3197a0d50b6e8b08f
Vanexcel <vanexcel.investments@gmail.com>
2024-02-12 15:14:06 +0000

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+
+import './Navbar.scss'
+
+
+
+export default function Navbar() {
+  return (
+    <div className='navbar'> 
+   
+    <div className="container">
+        <div className="logo">
+            <span className='text'>fiverr</span>
+            <span className='dot'>.</span>
+
+        </div>
+        <div className="links">
+        <span>Fiverr Business</span>
+        <span>Explore</span>
+        <span>English</span>
+        <span>Become a seller</span>
+        <span>Sign in</span>
+        <button className='btn-join'>Join</button>
+
+        </div>
+    </div>
+    
+    </div>
+  )
+}
